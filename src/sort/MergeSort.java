41cdd7c6d4d4e2afc7258f8dd9a1e52beaa7629d
Saehyun Kim <saehyun91@gmail.com>
2015-10-25 19:13:46 +0900

    add Merge Sort

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,5 +1,42 @@
 package sort;
 
 public class MergeSort {
-	
+	private void merge(int[] list, int left, int mid, int right) {
+		int[] mergeList = new int[list.length];
+		int idxOfLeftSortList = left;
+		int idxOfRightSortList = mid + 1;
+		int idxOfList = left;
+
+		while (idxOfLeftSortList <= mid && idxOfRightSortList <= right) {
+			if (list[idxOfLeftSortList] <= list[idxOfRightSortList]) {
+				mergeList[idxOfList++] = list[idxOfLeftSortList++];
+			} else {
+				mergeList[idxOfList++] = list[idxOfRightSortList++];
+			}
+		}
+
+		if (idxOfLeftSortList > mid) {
+			for (int i = idxOfRightSortList; i <= right; i++) {
+				mergeList[idxOfList++] = list[i];
+			}
+		} else {
+			for (int i = idxOfLeftSortList; i <= mid; i++) {
+				mergeList[idxOfList++] = list[i];
+			}
+		}
+
+		for (int i = left; i <= right; i++) {
+			list[i] = mergeList[i];
+		}
+
+	}
+
+	public void mergeSort(int[] list, int left, int right) {
+		if (left < right) {
+			int mid = (left + right) / 2;
+			mergeSort(list, left, mid);
+			mergeSort(list, mid + 1, right);
+			merge(list, left, mid, right);
+		}
+	}
 }
--- src/sort/test/SortTest.java
+++ src/sort/test/SortTest.java
@@ -1,5 +1,6 @@
 package sort.test;
 
+import sort.MergeSort;
 import sort.QuickSort;
 
 public class SortTest {
@@ -12,11 +13,13 @@ public class SortTest {
 
 	public static void main(String[] args) {
 		QuickSort qs = new QuickSort();
+		MergeSort ms = new MergeSort();
 
 		int[] list = { 5, 1, 3, 4, 10, 6, 6, 7, 8 };
 
 		listPrint(list);
-		qs.quickSort(list, 0, list.length - 1);
+		//		qs.quickSort(list, 0, list.length - 1);
+		ms.mergeSort(list, 0, list.length - 1);
 		listPrint(list);
 	}
 
