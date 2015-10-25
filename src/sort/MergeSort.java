4915ebfe6d50c3547503270aad7cc46d3a87d98d
Saehyun Kim <saehyun91@gmail.com>
2015-10-25 19:19:53 +0900

    modify comment

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -1,5 +1,15 @@
 package sort;
 
+/**
+ * Time complexity
+ * Best case: O(nlogn) or O(n)
+ * Worst case: O(nlogn)
+ * Average case: O(nlogn)
+ *
+ * 장점: 데이터의 분포에 영향을 덜 받음 -> 최선, 평균, 최악의 시간복잡도가 같음
+ * 단점: 임시 배열이 필요함 -> 레코드들의 크기가 큰 경우 시간 낭비
+ * -> 레코드를 연결 리스트로 구성하여 합병 정렬시 링크 인덱스만 변경
+ */
 public class MergeSort {
 	private void merge(int[] list, int left, int mid, int right) {
 		int[] mergeList = new int[list.length];
--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -8,7 +8,7 @@ package sort;
  *
  * 속도가 빠름, 추가 메모리 공간 불필요
  * 이미 정렬된 리스트에 대해서는 수행시간 오래걸림
- * -> 불균형 분할 방지하기 위해 pivot을 중간 값으로 ㅅ너택(median of three)
+ * -> 불균형 분할 방지하기 위해 pivot을 중간 값으로 선택(median of three)
  */
 public class QuickSort {
 
